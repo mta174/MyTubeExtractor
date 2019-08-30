1af9503331469111720c83215aeb99569ce12ac3
minhnv <minhnguyenvan@vccorp.vn>
2019-08-30 11:55:40 +0700

    delete

--- extractor/src/main/java/org/video/playtube/extractor/listdata/ListDataInfoItem.java
+++
@@ -1,9 +0,0 @@
-package org.video.playtube.extractor.listdata;
-
-import org.video.playtube.extractor.InfoItem;
-
-public class ListDataInfoItem extends InfoItem {
-    public ListDataInfoItem(int serviceId, String url, String name) {
-        super(InfoType.LISTDATA, serviceId, url, name);
-    }
-}
--- extractor/src/main/java/org/video/playtube/extractor/listdata/ListDataInfoItemExtractor.java
+++
@@ -1,7 +0,0 @@
-package org.video.playtube.extractor.listdata;
-
-import org.video.playtube.extractor.InfoItemExtractor;
-
-public interface ListDataInfoItemExtractor extends InfoItemExtractor {
-
-}
